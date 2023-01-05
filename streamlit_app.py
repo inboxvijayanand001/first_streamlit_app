25cea528c0b772adeae558e2b39603b4b0a513cc
Vijay Anand <122031214+inboxvijayanand001@users.noreply.github.com>
2023-01-05 20:12:39 +0530

    Update streamlit_app.py
    rectified error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
 
-Streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
-Streamlit.text('Omega 3 & Blueberry Oatmeal')
-Streamlit.text('Kale, Spinach & Rocket Smoothie')
-Streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
