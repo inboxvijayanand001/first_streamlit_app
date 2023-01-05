acfbf5b7952cc6f44b1a1a3b216fd3ed2b2ad798
Vijay Anand <122031214+inboxvijayanand001@users.noreply.github.com>
2023-01-05 23:07:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ import pandas
 fruit=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-my_fruit_list = fruit.set_index('Fruit')
+fruit = fruit.set_index('Fruit')
 
 # Display the table on the page.
 streamlit.write(fruit)
